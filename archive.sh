1dd26399ed375ccf60900d7c875e1d41e46e66fb
jun <jun@git.com>
2018-01-08 15:38:43 +0900

    aws用に調整中

--- archive.sh
+++ archive.sh
@@ -8,6 +8,7 @@ mkdir -p $DEST_DIR/go/bin $DEST_DIR/go/pkg $DEST_DIR/go/src/github.com/junpaymen
 git clone --depth 1 $REP_URL $DEST_DIR/go/src/github.com/junpayment/gin_labo
 
 cp -a $DEST_DIR/go/src/github.com/junpayment/gin_labo/.ebextensions $DEST_DIR/go/
+cp $DEST_DIR/go/src/github.com/junpayment/gin_labo/build.sh $DEST_DIR/go/
 cp $DEST_DIR/go/src/github.com/junpayment/gin_labo/Buildfile $DEST_DIR/go/
 cp $DEST_DIR/go/src/github.com/junpayment/gin_labo/Procfile $DEST_DIR/go/
 
