e14366ce8a34762d316a3e06a05f47e3e9c5c696
jun <jun@git.com>
2018-01-08 16:28:33 +0900

    aws用に調整中

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ GOBIN=/var/app/current/bin
 PATH=$PATH:$GOBIN
 
 curl https://glide.sh/get | sh
+
 cd $GOPATH/src/github.com/junpayment/gin_labo/;
 glide install;
-cd $GOPATH/src/github.com/junpayment/gin_labo/vendor/github.com/suapapa/go_sass/libsass;
-./configure;
-make;
-sudo make install;
+cd $GOPATH/src/github.com/junpayment/gin_labo/vendor/github.com/suapapa/go_sass/
+sh ./install_libsass.sh
+
 cd $GOPATH
 go build github.com/junpayment/gin_labo
